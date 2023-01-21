a8b1ceb7f21d650524c8f09e960069fc1bac0434
Vasil Atanasov <119658211+Atanasov-Vasil@users.noreply.github.com>
2023-01-21 11:07:56 +0200

    Add files via upload
    new version of the game: added lizard and Spock as in the Big Bang Theory series

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -4,6 +4,8 @@ from colorama import Fore
 rock = 'Rock'
 paper = 'Paper'
 scissors = 'Scissors'
+lizard = 'Lizard'
+spock = 'Spock'
 
 
 def player_chooses(some_char):
@@ -13,17 +15,25 @@ def player_chooses(some_char):
         player = paper
     elif some_char == 's':
         player = scissors
+    elif some_char == 'l':
+        player = lizard
+    else:
+        player = spock
     return player
 
 
 def computer_chooses():
-    computer_random = random.randint(1, 3)
+    computer_random = random.randint(1, 5)
     if computer_random == 1:
         computer = rock
     elif computer_random == 2:
         computer = paper
-    else:
+    elif computer_random == 3:
         computer = scissors
+    elif computer_random == 4:
+        computer = lizard
+    else:
+        computer = spock
     return computer
 
 
@@ -31,9 +41,11 @@ wins = 0
 draws = 0
 loses = 0
 
-command = input('Choose [r]ock, [p]aper or [s]cissors: ')
+print(Fore.BLUE + 'This is a game of Rock/Paper/Scissors/Lizard/Spock!')
+print(Fore.BLUE + 'For exit, type "end"!')
+command = input(Fore.RESET + 'Choose [r]ock, [p]aper, [s]cissors, [l]izard or [v] for Spock: ')
 while command != 'end':
-    if command == 'r' or command == 'p' or command == 's':
+    if command == 'r' or command == 'p' or command == 's' or command == 'v' or command == 'l':
         player_move = player_chooses(command)
         print(f'You chose {player_move}')
         computer_move = computer_chooses()
@@ -41,9 +53,11 @@ while command != 'end':
         if player_move == computer_move:
             print(Fore.YELLOW + 'Draw!')
             draws += 1
-        elif (player_move == rock and computer_move == scissors) \
-                or (player_move == scissors and computer_move == paper) \
-                or (player_move == paper and computer_move == rock):
+        elif (player_move == rock and (computer_move == scissors or computer_move == lizard)) \
+                or (player_move == scissors and (computer_move == paper or computer_move == lizard)) \
+                or (player_move == paper and (computer_move == rock or computer_move == spock)) \
+                or (player_move == lizard and (computer_move == paper or computer_move == spock)) \
+                or (player_move == spock and (computer_move == scissors or computer_move == rock)):
             print(Fore.GREEN + 'You win! Computer loses!')
             wins += 1
         else:
@@ -52,7 +66,7 @@ while command != 'end':
     else:
         print('Invalid choice. Try again, please...')
         pass
-    command = input(Fore.RESET + 'Choose [r]ock, [p]aper or [s]cissors: ')
+    command = input(Fore.RESET + 'Choose [r]ock, [p]aper, [s]cissors, [l]izard or [v] for Spock: ')
 else:
     print(f'Wins:Draws:Loses - {wins}:{draws}:{loses}')
-    print('Bye, bye!')
+    print('live long and prosper!')
